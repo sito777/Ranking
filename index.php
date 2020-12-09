8db8202c767e81394aea0260a863a79dc5fe6377
Ryux61 <70945989+Ryux61@users.noreply.github.com>
2020-12-09 14:25:23 +0100

    nieuwste versie

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ if (!$result)
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="css/styles.css">
-
+    <link rel="icon" href="images/favicon.png" type="image" sizes="16x16">
     <title>Ranking SD</title>
 </head>
 
@@ -78,7 +78,7 @@ if (!$result)
                         <td>
                             <div><?php echo $row["Naam"];?>  <?php echo $row["Tijd"];?> <?php echo $row["Niveau"];?>   </div>
                         </td>
-                    </tr>
+                    </tr>   
                     <tr>
                         
                     <?php 
